d231fb52822520626b226c197fb79f7a01284f41
Walter Cheng <walter.cheng@carleton.ca>
2026-06-17 23:35:27 -0400

    Fix dtk formula version source

--- Formula/dtk.rb
+++ Formula/dtk.rb
@@ -1,7 +1,8 @@
 class Dtk < Formula
   desc "Structured payload preprocessing layer for AI assistants"
   homepage "https://github.com/Walter0697/DTK"
-  url "https://github.com/Walter0697/DTK.git", branch: "main"
+  url "https://github.com/Walter0697/DTK/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "9d51e649e4a21539a1df10ddeee720de6265c8816e9bc6939d01e81b3a0e094f"
   license "MIT"
 
   depends_on "rust" => :build
